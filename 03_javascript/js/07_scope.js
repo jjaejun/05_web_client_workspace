4ca8945f38e9cd8e4152537ce2412b8653f671e9
jjaejun <leejj3240@gmail.com>
2023-11-20 18:50:03 +0900

    07_JavaScript_Scope 이해하기 (scope.js)
    - 함수 호출
    - const | let | var

---
+++ 03_javascript/js/07_scope.js
@@ -0,0 +1,50 @@
+// 전역
+let a = 10;
+console.log(a);
+
+function foo() {
+    // 지역
+    let b = 20;
+    console.log(b);
+
+    // 전역변수 접근
+    a = 30;
+}
+
+if(true) {
+    // 지역
+    let c = 99;
+    console.log(c);
+}
+
+for(let i = 0; i < 5; i++) {
+    // 지역
+    console.log(i);
+}
+
+// console.log(i); // Uncaught ReferenceError: i is not defined
+
+let j;
+for (let j = 0; j < 5; j++) {
+    console.log(j);
+}
+
+/**
+ * - const 블럭 스코프, 상수 선언시
+ * - let 블럭 스코프, 변수 선언시
+ * - var 함수 스코프(함수블럭만 인정), 변수 선언시
+ */
+function test() {
+    var a = 10;
+    var a = 20; // 문법오류 아님
+    console.log(a);
+
+    const b = 123;
+    // b = 456; // Uncaught TypeError: Assignment to constant variable.
+
+    let c = 1;
+    c = 2;
+    console.log(c);
+
+    // let c = 'ㅋㅋㅋ'; // Uncaught SyntaxError: Identifier 'c' has already been declared
+}
