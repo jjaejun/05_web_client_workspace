a18e76468670d72502ad9f5f91ef356ad7f1956b
jjaejun <leejj3240@gmail.com>
2023-11-24 19:07:29 +0900

    22_JavaScript_DOM 제어하기 (dom.js)
    - Node | Element 상속관계 이해하기
    - 새로운 Tag객체 생성하고 DOM Tree 추가하기
    - DOM Traversing 사용하기
    - @실습문제 (.wrapper하위 p태그의 내용을 🥦n으로 변경하기)

---
+++ 03_javascript/js/22_dom.js
@@ -0,0 +1,120 @@
+/**
+ * 모든 태그객체는 Node와 Element를 상속한다.
+ * 
+ * - Node
+ *  - TextNode
+ *  - CommentNode
+ *  - Element
+ * - Element (태그객체)
+ * 
+ * - Node#appendChild
+ * - Element#append
+ */
+document.querySelector("#btn1").addEventListener('click', () => {
+    const sample = document.querySelector("#sample");
+    console.dir(sample);
+
+    console.dir(Node.prototype);
+    console.dir(Element.prototype);
+});
+
+/**
+ * 새로운 요소
+ * 1. innerHTML
+ * 2. Tag객체로 만들어서 DOM Tree에 추가
+ */
+document.querySelector("#btn2").addEventListener('click', () => {
+    // document.querySelector("#target").innerHTML = '<h2>HelloWorld</h2>'
+    
+    const h2 = document.createElement("h2"); //<h2><h2>
+    const text = document.createTextNode("HelloWorld") //'HelloWorld'
+    h2.appendChild(text); // <h2>HelloWorld</h2>
+    document.querySelector("#target").appendChild(h2); // <div id="target"><h2>HelloWorld</h2></dev>
+    // 이벤트핸들러
+    h2.addEventListener("mouseover", () => {
+        console.log('🍉');
+    });
+});
+
+/**
+ * img 태그 생성
+ */
+document.querySelector("#btn3").addEventListener("click", () => {
+    const img = document.createElement("img"); // <img/>
+    img.src = '../../sample/image/hyunta.jpg';
+    img.alt = '예쁜 강아지 사진';
+    // img.width = 200;
+    // img.style = 'width: 200px';
+    img.style.width = '200px';
+    document.querySelector("#target").appendChild(img);
+});
+
+/**
+ * 요소 제거
+ * - remove() 스스로 제거
+ * - removeChild(child) 자식요소 제거
+ */
+document.querySelector("#btn4").addEventListener('click', () => {
+    const foo = document.querySelector("#foo");
+    foo.remove();
+});
+document.querySelector("#btn5").addEventListener('click', () => {
+    const list = document.querySelector("#list");
+    const foo = document.querySelector("#foo");
+    // list.removeChild(foo);
+    
+    // 모든 자식요소제거
+    while(list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+});
+
+/**
+ * DOM Traversing
+ * - 특정요소에서 DOM Tree를 따라 다음요소를 찾아가는 기술
+ * 
+ * Node
+ *  - 자식 firstChild | lastChild | childNodes
+ *  - 부모 parentNode
+ *  - 형제 nextSibling | previousSibling
+ * 
+ * Element (textNode를 찾을 일이 별로 없어 대부분 Element를 사용)
+ *  - 자식 firstElementChild | lastElementChild | children
+ *  - 부모 parentElement
+ *  - 형제 nextElementSibling | previousElementSibling
+ */
+document.querySelector("#btn6").addEventListener('click', () => {
+    const src = document.querySelector(".wrapper");
+    const p1 = src.firstElementChild;
+    const p5 = src.lastElementChild;
+    const p = src.children;
+    console.log(src, p1, p5, p);
+
+    p1.style.color = 'hotpink';
+});
+
+document.querySelector('#btn7').addEventListener('click', () => {
+    const src = document.querySelector("#p3");
+    const parent = src.parentElement;
+    console.log(src, parent);
+});
+
+document.querySelector('#btn8').addEventListener('click', () => {
+    const src = document.querySelector("#p3");
+    // const prevSibling = src.previousElementSibling; // #p2
+    const prevSibling = src.previousElementSibling.previousElementSibling; // #p1
+    // const nextSibling = src.nextElementSibling;
+    const nextSibling = src.nextElementSibling.nextElementSibling; // #p5
+    console.log(src, prevSibling, nextSibling);
+});
+
+document.querySelector("#btn9").addEventListener('click', () => {
+    const wrapper = document.querySelector('.wrapper');
+    // [...wrapper.children].forEach(() => {
+    Array.from(wrapper.children).forEach((child, index) => {
+        console.log(child, index);
+        const newText = document.createTextNode(`🥦${index + 1}`);
+        child.removeChild(child.firstChild); // Node계열의 속성 (text, comment, Element)
+        child.appendChild(newText);
+    });
+});
